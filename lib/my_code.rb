125fe5f1eefeb017fad91121e2702f4b5053be8a
Viachaslau Pashaliuk <v.pashaliuk@gmail.com>
2019-09-10 04:38:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -27,3 +27,12 @@ def reduce_to_total(source_array, starting_point = 0)
   source_array.each{|num| starting_point = starting_point + num}
   return starting_point
 end
+
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return false if !source_array[i]
+    i += 1
+  end
+  return true
+end
