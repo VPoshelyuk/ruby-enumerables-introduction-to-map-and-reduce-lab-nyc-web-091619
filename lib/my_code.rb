b52fc84510933b4d549367ccad9b66d02bf94d5a
Viachaslau Pashaliuk <v.pashaliuk@gmail.com>
2019-09-10 04:40:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -29,10 +29,6 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-  i = 0
-  while i < source_array.length do
-    return false if !source_array[i]
-    i += 1
-  end
+  source_array.each{|element| return false if !element}
   return true
 end
