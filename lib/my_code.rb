ae7e5eef3b6023bdb81b9b84d9d5e51ee8379c23
Viachaslau Pashaliuk <v.pashaliuk@gmail.com>
2019-09-10 04:28:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
 # My Code here....
+def map_to_negativize(source_array)
+  new_array = []
+  source_array.each{|num| new_array << num* -1}
+  new_array
+end
+
+def map_to_no_change(source_array)
+  new_array = []
+  source_array.each{|num| new_array << num}
+  new_array
+end
+
+def map_to_double(source_array)
+  new_array = []
+  source_array.each{|num| new_array << num * 2}
+  new_array
+end
+
+def map_to_square(source_array)
+  new_array = []
+  source_array.each{|num| new_array << num ** 2}
+  new_array
+end
+
+def reduce_to_total(source_array, starting_point)
+  source_array.reduce(starting_point) { |sum, num| sum + num}
+end
